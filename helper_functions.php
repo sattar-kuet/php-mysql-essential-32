afecc29a187b069eabd8a3f0df54c5bc941f0d43
Abdus Sattar Bhuiyan <sattar.kuet@gmail.com>
2020-02-23 15:08:24 +0600

    first commit

---
+++ helper_functions.php
@@ -0,0 +1,6 @@
+<?php 
+  function delivary_charge($weight){
+	  $devilary_charge = 20*$weight;
+	  echo $devilary_charge;
+  }
+?>
---
+++ home.php
@@ -0,0 +1,14 @@
+<?php require_once('helper_functions.php'); ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Include example</title>
+</head>
+<?php require_once('helper_functions.php'); ?>
+<body>
+<?php 
+
+  delivary_charge(2.5);
+?>
+</body>
+</html>
